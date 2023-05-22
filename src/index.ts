a923e115ff576ea36d443fe6276dd307c18c8218
pravinpoudel <prvnpoudel4@gmail.com>
2023-05-21 20:48:11 -0600

    webgpu webpack and typescript project setup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./main.css";
+
+console.log("hello");
+console.log("hello1");
+
+console.log(navigator.gpu);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    port: 8080,
+    static: path.resolve(__dirname, "dist"),
+    hot: true,
+  },
+  devtool: "cheap-module-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+};
